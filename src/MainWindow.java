2b4d295e0a8849fc0e719d3894d2b1f533481044
Lilian Soltan <liliansoltan50@gmail.com>
2021-12-25 21:56:19 +0300

    New files added

---
+++ src/MainWindow.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+/**JFrame este clasa principala , de la care mosteneste fiecare
+clasa care doreste sa fie fereastra
+ */
+public class MainWindow extends JFrame{
+
+    public MainWindow(){
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,350);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+}
